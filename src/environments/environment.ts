e744bcf19d539838e4265f3ad2ca255f554604f7
sera419 <seraphinenyemb@gmail.com>
2024-09-15 21:58:40 +0100

    Ajout produit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-//import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 
 export const environment = {
   production: false,
@@ -31,7 +31,7 @@ const firebaseConfig = {
 };*/
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//const app = initializeApp(firebaseConfig);
 
 /*
  * For easier debugging in development mode, you can import the following file
